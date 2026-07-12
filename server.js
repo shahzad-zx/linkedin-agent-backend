a03fbb2199f092e76373bf2f8cbcbbfb1776f744
kitsune-17051 <mdshahzad17051@gmail.com>
2026-07-12 22:19:02 +0530

    update latest

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
+// const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
+const CLIENT_URL = "https://generate-draft.netlify.app/" || "http://localhost:5173";
 
 app.use(cors({ origin: CLIENT_URL, credentials: true }));
 app.use(express.json());
